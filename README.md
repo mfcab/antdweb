19ccf485441a2c4ee23fdd994cb92d64f8db57d6
1192927413@qq.com <1192927413@qq.com>
2019-04-28 19:52:41 +0800

    init

--- README.md
+++
@@ -1,2 +0,0 @@
-# antdweb
-this is antd study project
---
+++ src/pages/products.js
@@ -0,0 +1,29 @@
+import {
+	connect
+} from 'dva';
+import ProductList from '../components/ProductList';
+import styles from './products.css';
+
+const Products = ({
+	dispatch,
+	products
+}) => {
+	function handleDelete(id) {
+		dispatch({
+			type: 'products/delete',
+			payload: id,
+		});
+	}
+	return (
+		<div>
+      <h2>List of Products</h2>
+      <ProductList onDelete={handleDelete} products={products} />
+    </div>
+	);
+};
+
+export default connect(({
+	products
+}) => ({
+	products,
+}))(Products);
